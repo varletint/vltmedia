2ea0ff393a72ba009df42d39aeb9235a0d64f844
varletint <intvarlet@gmail.com>
2024-04-04 19:16:50 +0100

    Create page and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import SignUP from "./pages/SignUP";
+import SignIn from "./pages/SignIn";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
-  return <h1 className="text-3xl text-green-400">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<SignUP />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+    // <About></About>
+  );
 }
---
+++ client/src/pages/SignUP.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SignUP() {
+  return <div className="text-3xl bg-lime-400 p-1 m-6">SignUP</div>;
+}
