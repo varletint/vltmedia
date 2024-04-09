bf0fe752b7abcaac8cee17675083e6e641889e3e
varletint <intvarlet@gmail.com>
2024-04-09 23:01:52 +0100

    Connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+// const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb conneted");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
